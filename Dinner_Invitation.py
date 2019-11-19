a22d7988ebdcc5e56ba240de511b5d156d87549d
Galih Kamulyan <41816380+Nagasasra@users.noreply.github.com>
2019-11-20 03:47:45 +0700

    Add files via upload
    Not finished yet

---
+++ Dinner_Invitation.py
@@ -0,0 +1,25 @@
+#the list of the very first people that were invited
+original_people=["Natakusuma","Ismayana","Anggadirja","Bimantara"]
+
+#the actual invitation
+print("\n"+original_people[0]+", you are an awesome person to the point that you deserve to have a seat during my dinner party! Just come here already!")
+print(original_people[1]+", you were so kind that you treated me to one of the most memorable dinner last month! now this is my turn, I owe you after all.")
+print(original_people[2]+", do you still remember me old friend? I am currently inviting all these people that we've known for ages, so let's go have dinner together, shall we? It's been too long mate.")
+print(original_people[3]+", I still remember you kicking my pupppies as clear as it was yesterday, but I forgive you. Let's become friends again, so please have dinner with us.")
+print("\n"+original_people[0]+" can't come unfortunately. He claimed he just got mauled by a tiger in the middle of the city. Sounds ridiculous. I guess he's not as awesome as I previously thought.")
+print(original_people[3]+" took grave offense to my invitation and swore to kick my puppies again. Did I said something rude in my letter though? I don't know and probably never will.")
+
+#people in which tragedies befell them so they couldn't come, or do they?
+not_coming_attacked=original_people[0]
+not_coming_insulted=original_people[3]
+
+#the list of people invited to replace those who couldn't come
+new_people=["Wiranugraha","Harumbraja"]
+
+#the actual invitation
+print("\n"+new_people[0]+", bro, did you hear the news? "+not_coming_attacked+" apparently got attacked by some fearsome beast. He was supposed to come but now that happened so why don't you come here?")
+print(new_people[1]+", hey! Luckily for you "+not_coming_insulted+" decided not to come so there are empty tables here. You said you wanna come, so, are you coming? Also since he's not here, why don't you bring your cute little bulldog?")
+
+
+
+
